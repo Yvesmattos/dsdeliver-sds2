38b537fd8946dc5112c304a776e2446c5d2473f3
Yves Mattos <yvesmattos@gmail.com>
2021-01-06 17:24:11 -0300

    Project Created

---
+++ backend/src/test/java/com/mattos/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattos.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
