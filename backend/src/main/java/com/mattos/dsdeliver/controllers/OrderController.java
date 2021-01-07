3513492a90c3c70011d7075b46b499fefb8af7e6
Yves Mattos <yvesmattos@gmail.com>
2021-01-07 20:32:29 -0300

    POST /orders

--- backend/src/main/java/com/mattos/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/mattos/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.mattos.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mattos.dsdeliver.dto.OrderDTO;
 import com.mattos.dsdeliver.services.OrderService;
@@ -27,4 +31,13 @@ public class OrderController {
 
 	}
 
+	@PostMapping
+	public ResponseEntity<OrderDTO> inser(@RequestBody OrderDTO dto) {
+
+		dto = service.insert(dto);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+
+	}
 }
